22ebbc89b6aa51bcfe5617f2cb14a066f19a4717
Vishnuvardhan Rajendran <vardhan16@hotmail.com>
2017-07-04 21:43:13 +0200

    changed 1 to 2

--- example.py
+++ example.py
@@ -6,7 +6,7 @@ def print_time( threadName, delay):
    count = 0
    while count < 5:
       time.sleep(delay)
-      count += 1
+      count += 2
       print "%s: %s" % ( threadName, time.ctime(time.time()) )
 
 # Create two threads as follows
