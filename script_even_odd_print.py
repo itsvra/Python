b191f2ed4a0ba5091f577994fa437dd19a56ae1c
itsvra <vardhan16@hotmail.com>
2018-01-30 11:06:26 +0100

    script to print even or odd numbers
    my first commit

---
+++ script_even_odd_print.py
@@ -0,0 +1,17 @@
+# print even number from 0 to 200
+
+
+for x in range(0,200):
+  if x == 200:
+    break
+  elif x % 2 == 0:
+    print x
+    
+# print odd number from 0 to 200 
+
+
+for x in range(0,200):
+  if x == 200:
+    break
+  elif x % 2 == 1:
+    print x
